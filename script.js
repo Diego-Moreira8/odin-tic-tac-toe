95419a9bb56f5908d52fc26e0a5a26e6bf58cb74
Diego Moreira <diego.moreira529@gmail.com>
2022-09-20 11:04:58 -0300

    Created updateBoard()

--- script.js
+++ script.js
@@ -2,11 +2,25 @@ const gameBoard = {
   board: ["", "", "", "", "", "", "", "", ""],
 };
 
+const boardPositions = document.querySelectorAll("main div");
+boardPositions.forEach((position) => {
+  position.addEventListener("click", updateBoard);
+});
+
+function updateBoard() {
+  for (let position of boardPositions) {
+    position.innerText =
+      gameBoard.board[parseInt(position.getAttribute("data-position")) - 1];
+  }
+}
+
 const PlayerFactory = (name, mark) => {
   const getName = () => name;
   const getMark = () => mark;
 
-  const play = () => {};
+  const play = () => {
+    console.log(mark);
+  };
 
   return { getName, getMark, play };
 };
