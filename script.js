42174b4bbe4650c7a43c06a7d0401117c8dd5271
Diego Moreira de Oliveira <diego.m.oliveira@una.br>
2022-09-19 21:44:29 -0300

    Print board

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
-// Creates the Game Board
-// (IIFE (Immediately Invoked Function Expression))
-const gameBoard = (() => new Array(9))();
+const boardPositions = document.querySelectorAll("main div");
+
+const gameBoard = ["", "", "", "", "", "", "", "", ""];
+
+function updateBoard() {
+  for (let position = 0; position < 9; position++) {
+    boardPositions[position].innerText = gameBoard[position];
+  }
+}
 
 const Player = (name, mark) => {
   return { name, mark };
