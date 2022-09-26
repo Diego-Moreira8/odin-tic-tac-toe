b50432c23895fbdd7bbff7b04f8dd19c254126d9
Diego <diego.moreira529@gmail.com>
2022-09-26 18:28:53 -0300

    Refactoring searchScore

--- script.js
+++ script.js
@@ -18,6 +18,13 @@ const gameFlow = (() => {
   const player1 = Player("Jogador(a)", "X");
   const player2 = Player("Jogador(a)", "O");
 
+  const playerScoreDisplay = document.querySelectorAll(".player-score");
+
+  const updateScoreDisplay = () => {
+    playerScoreDisplay[0].innerText = player1.getScore();
+    playerScoreDisplay[1].innerText = player2.getScore();
+  };
+
   let currentPlayer = player1;
   const switchCurrentPlayer = () => {
     currentPlayer === player1
@@ -26,7 +33,7 @@ const gameFlow = (() => {
   };
   const getCurrentPlayer = () => currentPlayer;
 
-  return { getCurrentPlayer, switchCurrentPlayer };
+  return { getCurrentPlayer, switchCurrentPlayer, updateScoreDisplay };
 })();
 
 const gameBoard = (() => {
@@ -68,29 +75,29 @@ const gameBoard = (() => {
 
   const searchScore = (mark) => {
     if (board[0] === mark && board[1] === mark && board[2] === mark) {
-      blockBoard();
       highlightMarks(0, 1, 2);
+      return true;
     } else if (board[3] === mark && board[4] === mark && board[5] === mark) {
-      blockBoard();
       highlightMarks(3, 4, 5);
+      return true;
     } else if (board[6] === mark && board[7] === mark && board[8] === mark) {
-      blockBoard();
       highlightMarks(6, 7, 8);
+      return true;
     } else if (board[0] === mark && board[3] === mark && board[6] === mark) {
-      blockBoard();
       highlightMarks(0, 3, 6);
+      return true;
     } else if (board[1] === mark && board[4] === mark && board[7] === mark) {
-      blockBoard();
       highlightMarks(1, 4, 7);
+      return true;
     } else if (board[2] === mark && board[5] === mark && board[8] === mark) {
-      blockBoard();
       highlightMarks(2, 5, 8);
+      return true;
     } else if (board[0] === mark && board[4] === mark && board[8] === mark) {
-      blockBoard();
       highlightMarks(0, 4, 8);
+      return true;
     } else if (board[2] === mark && board[4] === mark && board[6] === mark) {
-      blockBoard();
       highlightMarks(2, 4, 6);
+      return true;
     }
   };
 
@@ -100,7 +107,11 @@ const gameBoard = (() => {
       .getMark();
     updateBoard();
     e.target.removeEventListener("click", gameBoard.insertMark); // Block position
-    searchScore(gameFlow.getCurrentPlayer().getMark());
+    if (searchScore(gameFlow.getCurrentPlayer().getMark())) {
+      blockBoard();
+      gameFlow.getCurrentPlayer().winRound();
+      gameFlow.updateScoreDisplay();
+    }
     gameFlow.switchCurrentPlayer();
     displayCurrentPlayer();
   };
